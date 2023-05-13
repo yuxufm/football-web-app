8a6c761e6c75775c894e8057a642682d4d46626f
Yusuf Abid <yusufabid.m@gmail.com>
2023-05-14 06:17:59 +0700

    create main page

---
+++ src/Controller/MainPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainPageController extends AbstractController
+{
+    #[Route('/', name: 'app_main_page')]
+    public function index(): Response
+    {
+        return $this->render('main_page/index.html.twig', []);
+    }
+}
